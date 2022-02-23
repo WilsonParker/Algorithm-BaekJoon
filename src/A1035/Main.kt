c05f854eb759d2de95ff499e4402aee90e54d85e
hare <xogus0790@naver.com>
2022-02-23 17:30:49 +0900

    update 1035

---
+++ src/A1035/Main.kt
@@ -0,0 +1,56 @@
+package A1035
+
+import kotlin.math.abs
+import kotlin.math.min
+
+private lateinit var board: Array<Array<Int>>
+
+fun main() {
+    val piece = ArrayList<Pair<Int, Int>>()
+    for (y in 0..4) {
+        val l = readLine()!!.toCharArray()
+        for (x in 0..4)
+            if (l[x] == '*')
+                piece.add(Pair(y, x))
+    }
+
+    var result = Int.MAX_VALUE
+    for (x in piece.indices) {
+        board = Array(5) { Array(5) { 0 } }
+        board[piece[x].first][piece[x].second] = 1
+        piece.forEachIndexed { index, i ->
+            if (index != x)
+                move(i, piece[x])
+        }
+        result = min(result, board.sumOf { it.sum() } - piece.size)
+    }
+    print(result)
+}
+
+private fun move(i: Pair<Int, Int>, target: Pair<Int, Int>) {
+    val f = abs(target.first - i.first)
+    val s = abs(target.second - i.second)
+
+    if (board[target.first][target.second] == 0)
+        board[target.first][target.second] = f + s + 1
+    else {
+        move(
+            i,
+            if (f > s)
+                Pair(
+                    if (target.first > i.first) target.first - 1 else target.first + 1,
+                    target.second
+                )
+            else if (f == s)
+                Pair(
+                    if (target.first > i.first) target.first - 1 else target.first + 1,
+                    if (target.second > i.second) target.second - 1 else target.second + 1,
+                )
+            else
+                Pair(
+                    target.first,
+                    if (target.second > i.second) target.second - 1 else target.second + 1,
+                )
+        )
+    }
+}
