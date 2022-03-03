e07994ff86f438e0bad52ab991705ed7ce1e7728
dev9163 <dev9163@linkareer.com>
2022-03-03 12:29:44 +0900

    add test

--- src/Explain.kt
+++ src/Explain.kt
@@ -1,15 +1,16 @@
 fun main() {
-    val cnt = 9
-    val size = 3
-    val list = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val input = readLine()!!.split(" ").map{ it.toInt()}
+    val cnt = input[0]
+    val size = input[1]
+    val list = readLine()!!.split(" ").map{ it.toInt()}.toMutableList()
     val temp = Array(size) { 0 }
 
     temp.forEachIndexed { index, i ->
         temp[index] = 1
     }
     temp[size - 1] = cnt - size + 1
-    val fnc = fun(list: Array<Int>, temp: Array<Int>): Int {
-        val cln = list.clone().toMutableList()
+    val fnc = fun(list: List<Int>, temp: Array<Int>): Int {
+        val cln = list.toMutableList()
         var max = 0
         temp.forEach {
             var sum = 0
@@ -41,4 +42,10 @@ fun main() {
     }
 
     print(result)
+
+//    1 1 7
+//    1 2 6
+//    2 1 6
+//    ...
+//    7 1 1
 }
