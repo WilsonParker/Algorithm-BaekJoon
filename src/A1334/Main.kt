2437872997ff0448a6356d56edd1e6babc120431
dev9163 <dev9163@linkareer.com>
2022-03-14 17:29:49 +0900

    Create Main.kt

---
+++ src/A1334/Main.kt
@@ -0,0 +1,32 @@
+package A1334
+
+fun main() {
+    val input = readLine()!!
+    val result = if (input.length == 1) {
+        input.toInt() + 1
+    } else {
+        execute(input)
+    }
+    print(result)
+}
+
+private fun execute(s: String): String {
+    val l = s.length;
+    val odd = l % 2;
+    val half = l / 2;
+
+    val front = s.slice(IntRange(0, half - 1))
+    var back = s.slice(IntRange(half + odd, l - 1))
+
+    if (front.toInt() > back.toInt()) {
+        back = front.reversed()
+    } else {
+        return execute((s.toInt() + 1).toString())
+    }
+
+    return if (odd == 1) {
+        front + s.slice(IntRange(half, half)) + back
+    } else {
+        front + back
+    }
+}
