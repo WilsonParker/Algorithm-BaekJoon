610f2bdba551d4e59511069d6834f928050b4f6a
dev9163 <dev9163@linkareer.com>
2022-03-16 14:55:42 +0900

    update 1334 Test2

--- src/A1334/Main.kt
+++ src/A1334/Main.kt
@@ -1,40 +1,28 @@
 package A1334
 
 fun main() {
-    val input = readLine()!!
+    var input = readLine()!!
     val result = if (input.length == 1) {
         input.toInt() + 1
     } else {
-        execute(input)
+        var l = input.length;
+        val b = l % 2 + l / 2
+        if (input.slice(IntRange(0, 0)).toInt() <= input.slice(IntRange(b, b)).toInt()) {
+            val arr = input.toCharArray().map { it.digitToInt() }.toMutableList()
+            var carry = 0;
+            for (i in l / 2 downTo 0) {
+                val temp = arr[i] + 1
+                arr[i] = temp % 10
+                carry = temp / 10
+                if (carry == 0) break;
+            }
+            input = (if (carry == 1) "1" else "") + arr.joinToString("")
+        }
+
+        l = input.length
+        val front = input.slice(IntRange(0, l / 2 - 1))
+        val mid = if (l % 2 == 1) input.slice(IntRange(l / 2, l / 2)) else ""
+        front + mid + front.reversed()
     }
     print(result)
-}
-
-private fun execute(s: String): String {
-    val l = s.length;
-    val odd = l % 2;
-    val half = l / 2;
-
-    val front = s.slice(IntRange(0, half - 1))
-    var back = s.slice(IntRange(half + odd, l - 1))
-
-    if (front.first() > back.first()) {
-        back = front.reversed()
-    } else {
-        return execute(inc(s))
-    }
-
-    return if (odd == 1) {
-        front + s.slice(IntRange(half, half)) + back
-    } else {
-        front + back
-    }
-}
-
-private fun inc(s: String): String {
-    return if (s.length > 8) {
-        inc(s.slice(IntRange(0, 7))) + inc(s.slice(IntRange(7, s.length - 1)))
-    } else {
-        (s.toInt() + 1).toString()
-    }
 }
--- src/A1334/Test2.kt
+++ src/A1334/Test2.kt
@@ -1,22 +1,28 @@
 package A1334
 
 fun main() {
-    val input = "1234567890123456789"
+    var input = "9999"
     val result = if (input.length == 1) {
         input.toInt() + 1
     } else {
-        execute(input)
+        var l = input.length;
+        val b = l % 2 + l / 2
+        if (input.slice(IntRange(0, 0)).toInt() <= input.slice(IntRange(b, b)).toInt()) {
+            val arr = input.toCharArray().map { it.digitToInt() }.toMutableList()
+            var carry = 0;
+            for (i in l / 2 downTo 0) {
+                val temp = arr[i] + 1
+                arr[i] = temp % 10
+                carry = temp / 10
+                if (carry == 0) break;
+            }
+            input = (if (carry == 1) "1" else "") + arr.joinToString("")
+        }
+
+        l = input.length
+        val front = input.slice(IntRange(0, l / 2 - 1))
+        val mid = if (l % 2 == 1) input.slice(IntRange(l / 2, l / 2)) else ""
+        front + mid + front.reversed()
     }
     print(result)
-}
-
-private fun execute(s: String): String {
-    val l = s.length;
-    val odd = l % 2;
-    val half = l / 2;
-
-    val front = s.slice(IntRange(0, half - 1))
-    var back = s.slice(IntRange(half + odd, l - 1))
-
-    return "test"
 }
