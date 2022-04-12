662d893d01eb3ce5cc06489a1cbae531c00ba21e
dev9163 <dev9163@linkareer.com>
2022-04-12 12:20:05 +0900

    complete 2435

--- src/A2435/Test2.kt
+++ src/A2435/Test2.kt
@@ -1,16 +1,17 @@
 package A2435
 
-fun main() {
-    val (c, d) = readLine()!!.split(" ").map { it.toInt() }
-    var l = arrayOf(0)
-    for (i in 0..c) {
-    }
-    var max = 0
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val c = nextInt()
+    val d = nextInt()
+    val l = IntArray(c)
+    var max = -100
+    for (i in 0 until c) l[i] = nextInt()
     for (i in 0..c - d) {
         var sum = 0
-        for (j in i until i + d)
-            sum += l[j]
-        max = if (max < sum) sum else max
+        for (j in i until i + d) sum += l[j]
+        max = if (max > sum) max else sum
     }
     print(max)
 }
