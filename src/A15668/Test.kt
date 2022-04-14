52a384a4352166a5af345ab3c82fc530ff1887a7
dev9163 <dev9163@linkareer.com>
2022-04-14 17:30:18 +0900

    Update Test.kt

--- src/A15668/Test.kt
+++ src/A15668/Test.kt
@@ -11,36 +11,22 @@ fun main() {
     var loop = true
     val m = n.length
     var f = n.first().digitToInt()
-    do {
-        al.add(f)
-        for (i in f downTo 1) {
-            for (j in 1..9) {
-                if (!al.contains(j)) {
-                    al.add(j)
-                    if (al.size == m) {
-                        println(al.joinToString("").toInt())
-                        for (k in 1..9) {
-                            if (!al.contains(k) && !bl.contains(k)) {
-                                bl.add(k)
-                                if (al.joinToString("").toInt() + bl.joinToString("").toInt() > n.toInt()) {
 
-                                }
-                            }
-                        }
+    for (i in f downTo 1) {
+        for (j in 9 downTo 1) {
+            if (!al.contains(j)) {
+                al.add(j)
+                if (al.size == f) {
+                    if (toInt(al) > n.toInt()) {
+                        
                     }
                 }
             }
         }
-        println(al.joinToString("").toInt())
-        f--
-        loop = false
-    } while (loop)
+    }
 
     println(al)
     println(bl)
-}
-
-private fun getInt(l: Int) {
 }
 
 private fun toInt(l: List<Int>): Int {
