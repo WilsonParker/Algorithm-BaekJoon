c91c181d12f74c59b27e5f6c4f29c30ae0e9b110
dev9163 <dev9163@linkareer.com>
2022-04-21 17:28:45 +0900

    Update Main.kt

--- src/A1018/Main.kt
+++ src/A1018/Main.kt
@@ -9,11 +9,12 @@ fun main() {
     var min = 64
     for (a in 0..n - 8) {
         for (b in 0..m - 8) {
-            val l = board.map { it.clone() }.toTypedArray().clone()
-            val c = fun(): Int {
+            val l1 = board.map { it.clone() }.toTypedArray().clone()
+            val l2 = board.map { it.clone() }.toTypedArray().clone()
+            val c = fun(l: Array<CharArray>): Int {
                 var r = 0
-                for (i in a until a + 8) {
-                    for (j in b until b + 8) {
+                for (i in a..a + 7) {
+                    for (j in b..b + 7) {
                         if (j > b) {
                             if (l[i][j - 1] == l[i][j]) {
                                 l[i][j] = rc(l[i][j])
@@ -29,12 +30,22 @@ fun main() {
                 }
                 return r
             }
-            val r1 = c()
-            l[a][b] = rc(l[a][b])
-            val r2 = c()
-            val r = if (r1 > r2) r2 else r1
-            min = if (min > r) r else min
+            val r1 = c(l1)
+            l2[a][b] = rc(l2[a][b])
+            val r2 = c(l2)
+            println("$r1 $r2")
+            val r3 = if (r1 > r2) r2 else r1
+            min = if (min > r3) r3 else min
         }
     }
     print(min)
 }
+
+//BBBBBBBB
+//BBBBBBBB
+//BBBBBBBB
+//BBBBBBBB
+//BBBBBBBB
+//BBBBBBBB
+//BBBBBBBB
+//BBBBBBBW
