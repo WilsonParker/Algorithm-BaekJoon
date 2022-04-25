3451655949f2602904b88bc23dd88c78b6d7b181
dev9163 <dev9163@linkareer.com>
2022-04-25 16:28:22 +0900

    update 14002

--- src/A14002/Test.kt
+++ src/A14002/Test.kt
@@ -2,20 +2,44 @@ package A14002
 
 fun main() {
     val n = readLine()!!.toInt()
-    val v = readLine()!!.split(" ").map { it.toInt() }
-    val d = ArrayList<Int>()
+    val arr = readLine()!!.split(" ").map { it.toInt() }
+    val v = arr.toMutableList()
+    val r = Array(n) { it }
 
-    d.add(0)
-    for (i in v.indices) {
-        for (j in d.size - 1 downTo 0) {
-            if (v[i] > d[j]) {
-                // println("${v[i]} ${d[j]} $i $j")
-                d.add(d[j] + 1)
-                break
+    for (i in 0 until n)
+        for (j in 0 until n)
+            if (v[i] < v[j]) {
+                val t = v[i]
+                v[i] = v[j]
+                v[j] = t
+                val idx = r[i]
+                r[i] = r[j]
+                r[j] = idx
             }
+
+    val f = fun(i: Int): ArrayList<Int> {
+        val l = ArrayList<Int>()
+        var tmp = i
+        l.add(r[tmp])
+        for (j in i until n) {
+            if (r[tmp] < r[j]) {
+                l.add(r[j])
+                tmp = j
+            }
+        }
+        return l
+    }
+
+    var max = 0
+    var l = ArrayList<Int>()
+    for (i in 0 until n) {
+        val tmp = f(i)
+        if (max < tmp.size || max == tmp.size && l[0] < tmp[0]) {
+            max = tmp.size
+            l = tmp
         }
     }
 
-    // println(d.contentDeepToString())
-    println(d.joinToString(","))
+    println(max)
+    print(l.map { arr[it] }.joinToString(" "))
 }
