db60af4ce4c62b18f71a62422c110533e2ee82a9
dev9163 <dev9163@linkareer.com>
2022-04-25 15:58:49 +0900

    add 14002

---
+++ src/A14002/Test.kt
@@ -0,0 +1,21 @@
+package A14002
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val v = readLine()!!.split(" ").map { it.toInt() }
+    val d = ArrayList<Int>()
+
+    d.add(0)
+    for (i in v.indices) {
+        for (j in d.size - 1 downTo 0) {
+            if (v[i] > d[j]) {
+                // println("${v[i]} ${d[j]} $i $j")
+                d.add(d[j] + 1)
+                break
+            }
+        }
+    }
+
+    // println(d.contentDeepToString())
+    println(d.joinToString(","))
+}
