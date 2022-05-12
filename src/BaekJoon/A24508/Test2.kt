1c2538f813c8d697bd4baac1e1c02b5da2571d10
dev9163 <dev9163@linkareer.com>
2022-05-12 10:29:00 +0900

    update 24508

--- src/BaekJoon/A24508/Test2.kt
+++ src/BaekJoon/A24508/Test2.kt
@@ -13,13 +13,17 @@ fun main() {
 //    println(test("6 1 0", "0 0 0 0 0 0")) //
 //    println(test("2 1 2", "0 0 0")) //
 //    println(test("8 6 9", "5 2 2 2 2 2 2 1")) // YES
-    println(test("8 6 9", "5 2 2 2 2 2 2 1")) // YES
+//    println(test("8 6 9", "5 2 2 2 2 2 2 1")) // YES
+    println(test("4 5 3", "4 4 4 3")) // YES
+//    println(test("4 5 4", "4 3 2 2")) //
 }
 
 private fun test(s1: String, s2: String): String {
     val (_, k, t) = s1.split(" ").map { it.toInt() }
     val v = s2.split(" ").map { it.toInt() }.sortedByDescending { it }
     println(v)
+    println(v.sum())
+    println(v.sum() / k)
     println(v.slice(0 until v.sum() / k))
     println(v.slice(0 until v.sum() / k).sumOf { k - it })
     return if (v.sum() % k == 0 && v.slice(0 until v.sum() / k).sumOf { k - it } <= t) "YES" else "NO"
