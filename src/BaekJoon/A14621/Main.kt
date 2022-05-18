d513a45b5c8ceeecfd4a86f39e232569079c40c1
dev9163 <dev9163@linkareer.com>
2022-05-18 17:24:43 +0900

    update 14621

--- src/BaekJoon/A14621/Main.kt
+++ src/BaekJoon/A14621/Main.kt
@@ -11,8 +11,6 @@ fun main() {
         b[v][u] = if (b[v][u] == 0 || d + 1 < b[v][u]) d + 1 else b[v][u]
     }
 
-    println(b.contentDeepToString())
-
     var v = Array(n) { -1 }
     fun visit(from: Int, to: Int) {
         if (from != to) {
@@ -27,10 +25,9 @@ fun main() {
         }
     }
 
-    repeat(n) {
+    print((0 until n).map {
         visit(it, it)
-        println(v.contentDeepToString())
-        println(v.sum())
         v = Array(n) { -1 }
-    }
+        v
+    }.filter { !it.contains(-1) }.forEach { println(it.contentDeepToString()) })
 }
