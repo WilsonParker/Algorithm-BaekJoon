91de4e944da50da2fdff29b959cf35e8cf4ba306
dev9163 <dev9163@linkareer.com>
2022-05-25 17:29:20 +0900

    Update Main.kt

--- src/BaekJoon/A13565/Main.kt
+++ src/BaekJoon/A13565/Main.kt
@@ -4,10 +4,12 @@ fun main() {
     val (n, m) = readLine()!!.split(" ").map { it.toInt() }
     val b = Array(n) { readLine()!!.toCharArray() }
     fun move(x: Int, y: Int) {
-        if (x in 0 until m && y < n && b[y][x] == '0') {
+        if (x in 0 until m && y in 0 until n && b[y][x] == '0') {
             b[y][x] = '2'
             move(x - 1, y)
+            move(x - 1, y - 1)
             move(x + 1, y)
+            move(x + 1, y - 1)
             move(x, y + 1)
         }
     }
@@ -15,3 +17,7 @@ fun main() {
         move(x, 0)
     print(if (b.last().contains('2')) "YES" else "NO")
 }
+
+ㅁ   ㅁ
+ㅁ ㅁ ㅁ
+  ㅁ
