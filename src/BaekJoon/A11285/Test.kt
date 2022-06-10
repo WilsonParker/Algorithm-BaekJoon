719c9eb942e81edcef818c2ac2803dc87d87730e
dev9163 <dev9163@linkareer.com>
2022-06-10 16:54:34 +0900

    add 11285

---
+++ src/BaekJoon/A11285/Test.kt
@@ -0,0 +1,12 @@
+package BaekJoon.A11285
+
+import kotlin.test.assertEquals
+
+fun main() {
+    assertEquals(test(arrayOf('ㄱ', 'ㅏ')), "가")
+}
+
+private fun test(c: Array<Char>): String {
+    c.forEach { it }
+    return ""
+}
