ede8c8657cf92f74f3ea79cd60393c36afd000b7
dev9163 <dev9163@linkareer.com>
2022-06-10 11:33:03 +0900

    update 25200

--- src/BaekJoon/A25200/Test.kt
+++ src/BaekJoon/A25200/Test.kt
@@ -1,22 +1,54 @@
 package BaekJoon.A25200
 
+import kotlin.test.assertEquals
+
 fun main() {
-    val (n, m) = arrayOf(5, 4)
-    val r = arrayOf(
-        arrayOf(1, 3),
-        arrayOf(3, 2),
-        arrayOf(4, 3),
-        arrayOf(2, 4),
+    assertEquals(
+        test(
+            5, 4,
+            arrayOf(
+                arrayOf(1, 3),
+                arrayOf(3, 2),
+                arrayOf(4, 3),
+                arrayOf(2, 4),
+            )
+        ), "4 4 4 3 5"
     )
 
-    fun move(d: Int, i: Int = 0): Int {
-        val idx = r.indexOf(r.find { it[0] == d })
-        return if (idx == m - 1 && idx > i)
-            r[idx][1]
-        else if (idx in 0 until m - 1)
-            move(r[idx][1], idx)
-        else d
+    assertEquals(
+        test(
+            5, 4,
+            arrayOf(
+                arrayOf(1, 3),
+                arrayOf(3, 2),
+                arrayOf(4, 3),
+                arrayOf(2, 4),
+            )
+        ), "4 4 4 3 5"
+    )
+}
+
+private fun test(n: Int, m: Int, r: Array<Array<Int>>): String {
+    val ia = Array(m) { 0 }
+    r.forEachIndexed { i, ints -> ia[ints[0] - 1] = i }
+    r.sortBy { it[0] }
+    println(r.contentDeepToString())
+    println(ia.contentDeepToString())
+
+    fun move(d: Int, i: Int = -1): Int {
+        if (d > m) {
+            return d
+        }
+        val t = r[d - 1]
+        println("t ${t.contentDeepToString()}")
+        println("ia ${ia[t[0] - 1]} $i")
+        return if (ia[t[0] - 1] > i) {
+            move(r[t[1] - 1][0], ia[t[0] - 1])
+        } else {
+            t[0]
+        }
     }
 
-    print((1..n).map { move(it) }.joinToString(" "))
+    print(move(1))
+    return (1..n).map { move(it) }.joinToString(" ")
 }
