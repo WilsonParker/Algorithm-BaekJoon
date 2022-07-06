d5304155aedc03a5cffc0e3d9cd57bcd4cb8b671
dev9163 <dev9163@linkareer.com>
2022-07-06 17:15:31 +0900

    Update Test.kt

--- src/BaekJoon/A10384/Test.kt
+++ src/BaekJoon/A10384/Test.kt
@@ -1,4 +1,31 @@
 package BaekJoon.A10384
 
+import kotlin.test.assertEquals
+
 fun main() {
+    assertEquals(test(1, "The quick brown fox jumps over a lazy dog."), "Case 1: Pangram!")
+    assertEquals(test(2, "The quick brown fox jumps over a laconic dog."), "Case 2: Not a pangram")
+    assertEquals(
+        test(3, "abcdefghijklmNOPQRSTUVWXYZ-zyxwvutsrqpon   2013/2014      MLKJIHGFEDCBA"),
+        "Case 3: Double pangram!!"
+    )
+}
+
+private fun test(case: Int, input: String): String {
+    val c = ('a'..'z').map { it to 0 }
+    input.toCharArray().filter { it != ' ' }.groupBy { it }.map { it.value.count() }
+    for (a in 'a'..'z') {
+        println(a)
+    }
+    println(c)
+    return "Case $case: " + if (c[0] != c[c.size - 1])
+        "Not a pangram"
+    else {
+        when (c[0]) {
+            1 -> "Pangram!"
+            2 -> "Double pangram!!"
+            2 -> "Triple pangram!!!"
+            else -> ""
+        }
+    }
 }
