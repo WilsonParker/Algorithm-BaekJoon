a583a4b7b3d0587116479a8d91ce13e04d9c058d
dev9163 <dev9163@linkareer.com>
2022-07-15 16:02:13 +0900

    update 12785

--- src/BaekJoon/A12785/Main.kt
+++ src/BaekJoon/A12785/Main.kt
@@ -4,18 +4,22 @@ fun main() {
     fun rl() = readLine()!!.split(" ").map { it.toInt() - 1 }
     val (w, h) = rl()
     val (x, y) = rl()
-    fun find(s: Int, e: Int, t: Int, f: Int): Int {
-        val v = Array(w + 1) { Array(h + 1) { 0 } }
-        for (i in s + 1..t)
-            v[0][i] = v[0][i - 1] + 1
-        for (j in e + 1..f)
-            v[j][0] = v[j - 1][0] + 1
-        for (i in e..f)
-            for (j in s..t)
-                if (i > 0 && j > 0)
+    fun find(s: Int, e: Int, t: Int, f: Int): ULong {
+        val v = Array(f - e + 1) { Array(t - s + 1) { 0 } }
+        for (i in 1..t - s)
+            v[0][i] = 1
+        for (j in 1..f - e)
+            v[j][0] = 1
+        for (i in 0..f - e) {
+            for (j in 0..t - s) {
+                if (i > 0 && j > 0) {
                     v[i][j] = v[i - 1][j] + v[i][j - 1]
-        return v[f][t]
+                }
+            }
+        }
+        val r = v[f - e][t - s]
+        return if (r == 0) 1u else r.toULong()
     }
 
-    print(find(0, 0, x, y) * find(x, y, h, w) % 1000007)
+    print("${find(0, 0, x, y) * find(x, y, h, w) % 1000007u}")
 }
--- src/BaekJoon/A12785/Test.kt
+++ src/BaekJoon/A12785/Test.kt
@@ -1,33 +1,34 @@
 package BaekJoon.A12785
 
-import BaekJoon.Common.printlnArray
 import kotlin.test.assertEquals
 
 fun main() {
-    assertEquals(test("3 2", "2 2"), 2)
-    assertEquals(test("4 4", "2 2"), 10)
+    assertEquals(test("3 2", "2 2"), "2")
+    assertEquals(test("4 4", "2 2"), "12") // 10
+    assertEquals(test("5 5", "3 4"), "30")
+    assertEquals(test("5 5", "5 5"), "70")
+    assertEquals(test("200 200", "150 89"), "683569")
 }
 
-private fun test(input1: String, input2: String): Int {
+private fun test(input1: String, input2: String): String {
     val (w, h) = input1.split(" ").map { it.toInt() - 1 }
     val (x, y) = input2.split(" ").map { it.toInt() - 1 }
-    fun find(s: Int, e: Int, t: Int, f: Int): Int {
+    fun find(s: Int, e: Int, t: Int, f: Int): ULong {
         val v = Array(f - e + 1) { Array(t - s + 1) { 0 } }
-        for (i in s + 1..t)
-            v[0][i] = v[0][i - 1] + 1
-        for (j in e + 1..f)
-            v[j][0] = v[j - 1][0] + 1
-        for (i in e..f) {
-            for (j in s..t) {
+        for (i in 1..t - s)
+            v[0][i] = 1
+        for (j in 1..f - e)
+            v[j][0] = 1
+        for (i in 0..f - e) {
+            for (j in 0..t - s) {
                 if (i > 0 && j > 0) {
-                    println("$s $e $t $f")
                     v[i][j] = v[i - 1][j] + v[i][j - 1]
                 }
             }
         }
-        printlnArray(v)
-        return v[f][t]
+        val r = v[f - e][t - s]
+        return if (r == 0) 1u else r.toULong()
     }
 
-    return find(0, 0, x, y) * find(x, y, h, w) % 1000007
+    return "${find(0, 0, x, y) * find(x, y, h, w) % 1000007u}"
 }
